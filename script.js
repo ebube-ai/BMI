7ef7b692e8a36f4e6ba240ec857962ae5f2904b4
ebube-ai <oshioakpoghome@gmail.com>
2025-09-03 16:58:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ function CalculateBMI(){
             result.innerHTML = firstName.value + " " + lastname.value + ", your BMI value is " + bmiValue + "kg/m<sup>2</sup>" + " Your BMI falls under the underweight category, Try Eating more frequently. Slowly begin to eat 5 to 6 smaller meals during the day, While Choosing food with lots of nutrients. You can as well see your Doctor for further directives."
             result.style.color = "red";
         } else if(bmiValue >=18.5 && bmiValue <=24.9){
-            result.innerHTML = "Your BMI value is " + bmiValue + "kg/m<sup>2</sup>" + " Your BMI falls under the healthy category, focus on maintaining your healthy habits by regular physical activity, a balanced diet rich in fruits, vegetables, and whole grains, mindful eating."
+            result.innerHTML = firstName.value + " " + lastname.value + ", your BMI value is " + bmiValue + "kg/m<sup>2</sup>" + " Your BMI falls under the healthy category, focus on maintaining your healthy habits by regular physical activity, a balanced diet rich in fruits, vegetables, and whole grains, mindful eating."
             result.style.color ="green";
         } else if(bmiValue >=25 && bmiValue <=29.9){
-            result.innerHTML = "Your BMI value is" + bmiValue + "kg/m<sup>2</sup>" + " Your BMI falls under the overweight category, Try Getting more physical activity or exercise, Consider making some lifestyle choices, nutriton-wise." 
+            result.innerHTML = firstName.value + " " + lastname.value +  ", your BMI value is" + bmiValue + "kg/m<sup>2</sup>" +  "Your BMI falls under the overweight category, Try Getting more physical activity or exercise, Consider making some lifestyle choices, nutriton-wise." 
             result.style.color = "orange"
         } else if(bmiValue >=30){
-            result.innerHTML = "Your BMI value is" + bmiValue +  "kg/m<sup>2</sup>" + " Your BMI value falls under the obese category, Adopt healthy habits, such as eating more fruits and vegetables or being more physically active. Consult your physcian for more support."
+            result.innerHTML = firstName.value + " " + lastname.value + ", your BMI value is" + bmiValue +  "kg/m<sup>2</sup>" + " Your BMI value falls under the obese category, Adopt healthy habits, such as eating more fruits and vegetables or being more physically active. Consult your physcian for more support."
             result.style.color = "red";
         }
     }
